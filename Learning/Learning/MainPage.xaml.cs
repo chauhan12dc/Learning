6b0c6d6c288df7ff3891d07a2330ea85f6e2eb98
Windows10 <Windows10@CHAUHAN12>
2021-02-25 01:41:32 +0530

    .

---
+++ Learning/Learning/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Learning.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Learning
+{
+    public partial class MainPage : ContentPage
+    {
+        current c = new current();
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = c;
+            //Device.StartTimer(TimeSpan.FromSeconds(1), c.ct);
+           
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            var page = new Page1();
+            Navigation.PushAsync(new Page1());
+            
+        }
+        bool cts()
+        {
+            //lbldate.Text = DateTime.Now.ToString("hh:mm:ss");
+            return true;
+        }
+    }
+}
---
+++ Learning/Learning/ViewModel/current.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learning.ViewModel
+{
+    class current
+    {
+        public string currentDate { get; set; }
+        public current()
+        {
+            currentDate = DateTime.Now.ToString("hh:mm:ss");
+        }
+        public bool ct()
+        {    
+            return true;
+        }
+    }
+}
